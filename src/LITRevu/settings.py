a5617ee437d699ec34c6548fb9e882a08b79fd20
Narfyx <kevin.abribat@live.fr>
2024-04-17 19:31:34 +0200

    page connection html

--- src/LITRevu/settings.py
+++ src/LITRevu/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "LITRevu.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [f'{BASE_DIR}/LITRevu/templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- src/LITRevu/urls.py
+++ src/LITRevu/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from .views import index
 
 urlpatterns = [
+    path("", index, name="test_index"),
     path("admin/", admin.site.urls),
+    
 ]
